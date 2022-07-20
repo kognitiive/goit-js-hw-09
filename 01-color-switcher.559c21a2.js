719ac668193d8b595506b741d3689677c32d1a11
kognitiive <danchenkoevhenii@gmail.com>
2022-07-20 19:08:12 +0000

    Deploying to gh-pages from @ kognitiive/goit-js-hw-09@aaf55dca8d7a1f5d48dd4acb714905255d2fa46f 🚀

---
+++ 01-color-switcher.559c21a2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let o=null;t.addEventListener("click",(function(){o=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t=this,t.disabled=!0;var t})),e.addEventListener("click",(function(){clearInterval(o),e=t,e.disabled=!1;var e}));
+//# sourceMappingURL=01-color-switcher.559c21a2.js.map
---
+++ 01-color-switcher.766fda1d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t=this,t.disabled=!0;var t})),e.addEventListener("click",(function(){clearInterval(o),e=t,e.disabled=!1;var e}))}();
+//# sourceMappingURL=01-color-switcher.766fda1d.js.map
